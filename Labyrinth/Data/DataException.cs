967ffeec41bfdd277257223c3ff6249c9280c76e
kressecske <kressecske@gmail.com>
2016-10-10 23:01:19 +0200

    PostCommit to DataException ^^

---
+++ Labyrinth/Data/DataException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth.Data
+{
+    class DataException : Exception
+    {
+        public DataException(String message) : base(message) { }
+    }
+}
