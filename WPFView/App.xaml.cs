4972c4652533b9c081bab8dd13d57b454414faba
kressecske <kressecske@gmail.com>
2016-11-22 22:35:05 +0100

    WPF

---
+++ WPFView/App.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Labyrinth.Data;
+using Labyrinth.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFView.ViewModel;
+
+namespace WPFView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Fields
+        private Game _model;
+        private GameViewModel _viewModel;
+        private MainWindow _view;
+        ALevel level1 = new Level1();
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Alkalmazás példányosítása.
+        /// </summary>
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        #endregion
+
+        #region Application event handlers
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            // modell létrehozása
+            _model = new Game();
+            _model.gameEnd += new EventHandler<Boolean>(Model_GameOver);
+
+            _model.newGame(level1.gameSize, level1.gameBoard);
+
+            // nézemodell létrehozása
+            _viewModel = new GameViewModel(_model);
+            _viewModel.NewGame += new EventHandler(ViewModel_NewGame);
+            _viewModel.LoadGame += new EventHandler(ViewModel_LoadGame);
+
+
+            // nézet létrehozása
+            _view = new MainWindow();
+            _view.DataContext = _viewModel;
+            _view.Show();
+        }
+        #endregion
+
+        #region ViewModel event handlers
+        private void ViewModel_NewGame(object sender, EventArgs e)
+        {
+            _model.newGame(level1.gameSize, level1.gameBoard);
+        }
+        private void ViewModel_LoadGame(object sender, System.EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog(); // dialógusablak
+                openFileDialog.Title = "Pálya betöltése";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    // játék betöltése
+                    _model.loadLevel(openFileDialog.FileName);
+                }
+            }
+            catch (Labyrinth.Data.DataException)
+            {
+                MessageBox.Show("A fájl betöltése sikertelen!", "Labyrinth", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        #endregion
+
+        private void Model_GameOver(object sender, Boolean e)
+        {
+            if (e)
+            {
+                MessageBox.Show("You Won", "Game Finished");
+            }
+        }
+
+    }
+}
---
+++ WPFView/ViewModel/GameField.cs
@@ -0,0 +1,61 @@
+﻿using Labyrinth.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFView.ViewModel
+{
+    public class GameField : ViewModelBase
+    {
+        private FieldType _type;
+        private Boolean _visibility;
+
+
+        public Boolean Visibile
+        {
+            get { return _visibility; }
+            set
+            {
+                if (_visibility != value)
+                {
+                    _visibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Felirat lekérdezése, vagy beállítása.
+        /// </summary>
+        public FieldType Type
+        {
+            get { return _type; }
+            set
+            {
+                if (_type != value)
+                {
+                    _type = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Vízszintes koordináta lekérdezése, vagy beállítása.
+        /// </summary>
+        public Int32 X { get; set; }
+
+        /// <summary>
+        /// Függőleges koordináta lekérdezése, vagy beállítása.
+        /// </summary>
+        public Int32 Y { get; set; }
+
+        /// <summary>
+        /// Sorszám lekérdezése.
+        /// </summary>
+        //public Int32 Number { get; set; }
+
+    }
+}
